d3f28b44fcd01b70122ead6a3f0a762062ceb1fc
Diego Carvajal <diego.carvajal.1515@gmail.com>
2025-02-13 02:47:45 -0300

    Agregar integración del módulo Pokémon en el módulo Seed y mejorar el servicio Seed para insertar datos de Pokémon desde la API

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [PokemonModule]
+
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import axios, {AxiosInstance} from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
-
+  
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ){}
   
 
+
   async executeSeed() {
+
+    await this.pokemonModel.deleteMany({});
+    
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=151');
+
+    const pokemonsToInsert: {name : string, no: number}[] = [];
+
 
     data.results.forEach(({name, url}) => {
     const no: number = Number(url.split('/').filter(Boolean).pop());
-    console.log({name, no});
+
+    pokemonsToInsert.push({name, no});
     });
-    return data;
+
+    await this.pokemonModel.insertMany(pokemonsToInsert);
+    
+    return 'seed executed'; 
   }
 
 }
